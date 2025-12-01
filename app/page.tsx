8236e66aba51917fcc13321d6044e5755a33b660
Kalyan Manna <kalyanmanna439@gmail.com>
2025-12-02 00:13:46 +0530

    Update homepage layout and content adjustments

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 import { Button } from "@/components/ui/button";
-import Header from "./_components/Header";
 import Hero from "./_components/Hero";
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center">
-    <Header />
+    
     <Hero />
     <h1 className="font-game text-2xl">Build with 💗 By NextJS</h1>
     <Button variant="destructive">Click me!!</Button>
