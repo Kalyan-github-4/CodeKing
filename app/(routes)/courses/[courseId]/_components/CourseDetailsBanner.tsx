40c73e5ff4901324564eaaf00ec053800f0d9d24
Kalyan Manna <kalyanmanna439@gmail.com>
2025-12-08 00:01:27 +0530

    UI Update: Enhanced CourseDetailsBanner layout and visuals

--- app/(routes)/courses/[courseId]/_components/CourseDetailsBanner.tsx
+++ app/(routes)/courses/[courseId]/_components/CourseDetailsBanner.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Course } from "../../_components/CourseList";
 import { Skeleton } from "@/components/ui/skeleton";
 import Image from "next/image";
+import { Button } from "@/components/ui/button";
 
 type Props = {
   loading: boolean;
@@ -13,23 +14,28 @@ const CourseDetailsBanner = ({ loading, courseDetail }: Props) => {
   const bannerUrl =
     typeof courseDetail?.bannerImage === "string"
       ? courseDetail.bannerImage.trim()
-      : "/hero2.gif"; // (add a fallback image in public folder)
+      : "/hero2.gif"; 
 
   return (
     <div>
       {loading ? (
-        <Skeleton className="w-full h-[300px] rounded-2xl" />
+        <Skeleton className="w-full h-[300px] rounded-2xl mt-20" />
       ) : !courseDetail ? (
-        <Skeleton className="w-full h-[300px] rounded-2xl" />
+        <Skeleton className="w-full h-[300px] rounded-2xl mt-20" />
       ) : (
-        <div>
+        <div className="relative mt-20">
           <Image
             src={bannerUrl}
             alt={courseDetail.title}
             width={1400}
             height={300}
-            className="w-full h-[300px] object-cover rounded-2xl"
+            className="w-full h-[300px] object-cover "
           />
+          <div className="font-game absolute top-0 pt-15 h-full p-10 md:px-24 lg:px-36 bg-linear-to-r from-black/80 to to-white-50/50">
+            <h2 className="text-6xl">{courseDetail.title}</h2>
+            <p className="text-2xl text-gray-300 mt-3">{courseDetail.description}</p>
+            <Button variant={'pixel'} className="mt-7 w-36">Enroll Now</Button>
+          </div>
         </div>
       )}
     </div>
